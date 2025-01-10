1f55e7ec73f425b1e2de3ac9a31839704671687d
Fatia Razaqy <finixman2@gmail.com>
2025-01-10 19:18:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    // Mendapatkan elemen dengan id "welcomeMessage"
+    var messageElement = document.getElementById('welcomeMessage');
+    
+    // Mengubah gaya tampilan elemen menjadi block agar terlihat
+    messageElement.style.display = 'block';
+}
